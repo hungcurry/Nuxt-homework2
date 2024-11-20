e0dce56aa6c57008a14350ca9c2a84364d3bf649
hungcurry <ooopp42@gmail.com>
2024-11-20 12:58:01 +0800

    init

---
+++ src/layouts/default.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="template">
+    <AppHeader />
+    <main class="content">
+      <slot />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.template {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.content {
+  flex-grow: 1;
+}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div class="container">
+    <p>目前路由的路徑 - route.fullPath為 : {{ route.fullPath }}</p>
+    <h1 class="primary">
+      Page: 首頁
+    </h1>
+    <h2 class="title">
+      前台首頁
+    </h2>
+    <hr>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.primary {
+  color: $primary;
+}
+.title {
+  color: #e38d28;
+  font-size: $font-base;
+}
+</style>
